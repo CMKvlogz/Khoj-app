0bada3cd48b9ea16c69ef6dea70f35996f201d36
CMKvlogz <abdulhaseebaamir464@gmail.com>
2026-08-12 16:32:52 +0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import * as Sentry from "@sentry/react";
 import App from "./App.jsx";
 import "./index.css";
+
+Sentry.init({
+  dsn: "https://0505c0b75ada7c4bcea660b8b531b77b@o4511897604849664.ingest.de.sentry.io/4511897624445008",
+});
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
